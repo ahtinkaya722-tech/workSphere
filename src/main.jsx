e213872b92cdc148c838fd741f14ddedab529ce0
Ahtnkaya <ahtinkaya722@gmail.com>
2026-05-14 14:12:11 +0630

    Switch to HashRouter for GitHub Pages support

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import './index.css'
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
