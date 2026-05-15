491446595d09cc370771a5e5eeb0956aec5e7ed7
Ahtnkaya <ahtinkaya722@gmail.com>
2026-05-16 00:16:57 +0630

    Fix: Configure CORS for Vercel and GitHub Pages origins

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors({
+  origin: ["https://work-sphere-dun.vercel.app", "https://ahtinkaya722-tech.github.io"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true
+}));
+
 app.use(express.json());
-
-
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
